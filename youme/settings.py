d52e33b4189d8657f686a8fae331abc38dd1c943
youme <mrsimplelife@naver.com>
2021-03-08 20:40:39 +0900

    Add pagination

--- youme/settings.py
+++ youme/settings.py
@@ -125,4 +125,6 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
+    "PAGE_SIZE": 3,
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
 }
